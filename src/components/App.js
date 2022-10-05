8d12b90eee9e3c039b2fd60bff8e724d5e3ae0b1
kush002 <kushagrasingh543@gmail.com>
2022-10-05 14:05:42 +0530

    Add files via upload

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,50 @@
-import React from 'react'
+import React, { useState } from "react";
 
-import '../styles/App.css';
+import "../styles/App.css";
 
-
-const searchArray = ["Newton", "School", "Newton School", "React", "Preact", "Node", "Mongoose", "Angular", "Vue", "Apple", "Microsoft", "Netflix", "Meta"]
+const searchArray = [
+  "Newton",
+  "School",
+  "Newton School",
+  "React",
+  "Preact",
+  "Node",
+  "Mongoose",
+  "Angular",
+  "Vue",
+  "Apple",
+  "Microsoft",
+  "Netflix",
+  "Meta",
+];
 const App = () => {
+  const [searchWords, setSearchWords] = useState("");
+  const [showResult, setShowResult] = useState(false);
 
-  
+  const searchHandler = (e) => {
+    setSearchWords(e.target.value);
+    if (e.target.value.trim() !== "") {
+      setShowResult(true);
+    } else {
+      setShowResult(false);
+    }
+    searchWords = searchWords.toLocaleLowerCase;
+  };
+  const showData = searchArray.map((item) => {
+    if (item.toLowerCase().includes(searchWords.toLowerCase())) {
+      return <li>{showResult && item}</li>;
+    }
+  });
+
   return (
     <div id="main">
-      
+      <label htmlFor="search-input">Search</label>
+      <br />
+      <input id="search-input" type="text" onChange={searchHandler} />
+      <p>Result</p>
+      <ul>{showResult && showData}</ul>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
